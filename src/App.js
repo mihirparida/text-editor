df6f3792fd0f89a01e9ea2eecafb8c7ee5f2de91
Mihir <mihirranjan.kbl@gmail.com>
2023-09-20 01:50:23 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 
 function App() {
@@ -43,20 +43,20 @@ function App() {
   }
 return (
   <>
-    {/* <Router> */}
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} toggleColor={toggleColor} key={new Date()} />
       <Alert alert={alert} />
       <div className="container my-3">
-        {/* <Routes> */}
+        <Routes>
           {/* /users --> Component 1
         /users/home --> Component 2 */}
-          {/* <Route path="/about" element=<About mode={mode} /> /> */}
-          {/* <Route exact path="/"element= */}
-          <TextForm showAlert={showAlert} heading="Try TextEditor - word counter, character counter, remove extra spaces" mode={mode}  toggleColor={toggleColor}/>
+          <Route path="/about" element=<About mode={mode} /> />
+          <Route exact path="/"
+          element= <TextForm showAlert={showAlert} heading="Try TextEditor - word counter, character counter, remove extra spaces" mode={mode}  toggleColor={toggleColor}/>/>
           {/* /> */}
-        {/* </Routes> */}
+        </Routes>
       </div>
-    {/* </Router> */}
+    </Router>
   </>
 );
 }
